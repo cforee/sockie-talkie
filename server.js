608e3050f0a2529e08fc6c60083b206ff8325f56
cforee <cforee@gmail.com>
2014-10-17 03:36:46 -0700

    Update port after setting PORT env variable on heroku

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var io = require('socket.io').listen(process.env.PORT || 8080);
+var io = require('socket.io').listen(process.env.PORT);
 
 io.sockets.on('connection', function(socket){
     socket.on('msg', function(data){
